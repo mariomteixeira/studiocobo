296beba30665b6f35f9c0c32e6a8967dee1090f9
Mario Teixeira <mariomarcio12324@gmail.com>
2024-01-25 20:56:39 -0300

    Navbar + Favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-moss': '#4B6A47',
+      },
+    },
   },
   plugins: [],
 }
-
